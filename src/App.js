323aded0af9e28cfb8441f4b60751b5c1af180c3
chenbaruchchen <62347294+chenbaruchchen@users.noreply.github.com>
2021-12-27 12:32:38 +0200

    devlope web camre classifier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Sketch from "react-p5";
 import PRINT10 from "./projects/10PRINT/App";
-import MlFirst from "./projects/Machine-Learning/hellow-world/App";
+import Classifier from "./projects/Machine-Learning/image-classifier/App";
 export default function App() {
-  return <MlFirst />;
+  return <PRINT10 />;
 }
